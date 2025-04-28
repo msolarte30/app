bde82d512167174ef4966b039734ebc779c19644
msolarte30 <63171723+msolarte30@users.noreply.github.com>
2025-04-28 03:28:02 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host="localhost",
-        database="Inventarios",
-        user="postgres",
-        password="ingreso-1"
+        host="dpg-d07dhgs9c44c739ssq2g-a",
+        database="inventarios_m0he",
+        user="inventarios_m0he_user",
+        password="9YMkzbvMEPVJTjK3gT1jkqhyHSxbTv5p"
     )
     return conn
